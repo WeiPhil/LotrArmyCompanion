9852761c42994d664858208f46cd52142d8f4df1
Pierre Thévenet <thevenet.pierre@gmail.com>
2018-12-26 14:53:04 +0100

    database connection via envvar

--- webserver/settings.py
+++ webserver/settings.py
@@ -9,7 +9,7 @@ WEBSERVER_PORT = int(os.getenv('WEBSERVER_PORT', 3000))
 DATABASE_CFG = {
     'user': os.getenv('MYSQL_USER', 'root'),
     'password': os.getenv('MYSQL_PASSWORD', os.getenv('MYSQL_ROOT_PASSWORD', 'root')),
-    'host': 'database',
+    'host': os.getenv("MYSQL_HOST", 'database'),
     'port': 3306,
     'database': 'app'
 }
